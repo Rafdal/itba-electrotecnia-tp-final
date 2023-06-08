93206aecf8b64ab9247e25862379898f851583c2
Rafdal <rdalzotto@itba.edu.ar>
2023-06-08 17:37:44 -0300

    Anda

--- lecturaSpice.py
+++ lecturaSpice.py
@@ -20,20 +20,28 @@ for i in plot_VL_C1:
     symbol = data.varListInfo[i]["symbol"]
     print(f"Symbol: {symbol}")
     color = data.colors[symbol]
+
+    label = data.varListNames[i].upper()
+
     if data.is_montecarlo:
         for run in data.varListData[i]:
             ax1.plot(data.time, run, linewidth=0.5, color=color, alpha=0.2)
     else:
         if data.varListInfo[i]["symbol"] == "I":
             ax2 = ax1.twinx()
-            ax2.set_ylabel("Voltage (V)")
-            ax2.plot(data.time, data.varListData[i], linewidth=1.0, color=color, alpha=1.0, label=data.varListNames[i])
+            ax2.plot(data.time[0:200], data.varListData[i][0:200], linewidth=1.0, color=color, alpha=1.0, label=label)
         else:
-            ax1.plot(data.time, data.varListData[i], linewidth=1.0, color=color, alpha=1.0, label=data.varListNames[i])
+            ax1.plot(data.time[0:200], data.varListData[i][0:200], linewidth=1.0, color=color, alpha=1.0, label=label)
 
 
-ax1.set_ylabel("Current (A)")
+plt.title("Carga")
+ax2.set_ylabel("Current (A)")
+ax1.set_ylabel("Voltage (V)")
 ax1.set_xlabel('Time (s)')
 ax1.grid()
-ax1.legend()
+ax2.legend(loc="lower right")
+ax1.legend(loc="upper right")
+
+fig.subplots_adjust(right=0.85) # Adjust the right margin
+
 plt.show()
