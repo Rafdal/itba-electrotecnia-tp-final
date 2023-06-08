85f2ff68a791acfcffd833058a4738403682dde0
Rafdal <rdalzotto@itba.edu.ar>
2023-06-08 16:17:10 -0300

    Anda

--- lecturaSpice.py
+++ lecturaSpice.py
@@ -6,13 +6,14 @@ from backend.LTSpiceReader import ReadLTSpice
 
 # FILE INDEX TO OPEN
 # No especificar el indice para que pregunte
-data = ReadLTSpice(folder="data", idx=1)
+data = ReadLTSpice(folder="data", idx=0)
 data.info() # Imprime información del archivo LTSpice (variables, casos, etc)
 
+plot_VL_C1 = [0, 4]
 
 print(len(data.varListInfo))
 
-for i in range(len(data.varListInfo))[2:]:
+for i in plot_VL_C1:
 
     print(f"Plotting {data.varListNames[i]}")
 
@@ -24,8 +25,10 @@ for i in range(len(data.varListInfo))[2:]:
             plt.plot(data.time, run, linewidth=0.5, color=color, alpha=0.2)
 
     else:
-        plt.plot(data.time, data.varListData[i], linewidth=1.0, color=color, alpha=0.3)
+        plt.plot(data.time, data.varListData[i], linewidth=1.0, color=color, alpha=1.0, label=data.varListNames[i])
 
 plt.ylabel("Current (A)")
 plt.xlabel('Time (s)')
+plt.grid()
+plt.legend()
 plt.show()
