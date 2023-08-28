1253d73c3afdf022cb975affa90ab78d221f228b
Rafdal <rdalzotto@itba.edu.ar>
2023-08-27 23:07:49 -0300

    anda

--- utils/OpAmpLib.py
+++ utils/OpAmpLib.py
@@ -1,5 +1,6 @@
 import sympy as sp
 import numpy as np
+from plotTools import plotBode
 
 class OpAmp():
     def __init__(self, s = sp.symbols('s', real=False)) -> None:
@@ -26,7 +27,7 @@ class OpAmp():
         self.h = sp.simplify(self.k * err_a)
         return self.h
     
-    def pretty_print(self):
+    def pretty(self):
         num = sp.numer(self.get_h())
         den = sp.denom(self.get_h())
         poly = sp.Poly(den, self.s)
@@ -39,17 +40,36 @@ class OpAmp():
 
         poly = sp.Poly.from_list(coeffs, s)
 
+        h_pretty = sp.simplify(num/self.wb) / (sp.simplify(indep_term/self.wb) * poly.expr)
+        return h_pretty
+    
+    def pretty_print(self):
         print('H(s) = \n')
-        sp.pretty_print( sp.simplify(num/self.wb) / (sp.simplify(indep_term/self.wb) * poly.expr), wrap_line=False)
+        print(sp.pretty(self.pretty(), wrap_line=False))
 
-    def eval(self, symbols, values):
+    def eval(self, symbols, values, s_vals=None):
         self.h = self.get_h()
-        self.h = self.h.subs(symbols, values)
-        self.h_func = sp.lambdify(self.s, self.h, 'numpy')
-        return self.h_func
+        # add [a0, wb] to symbols and values
+        symbols = [*symbols, self.a0, self.wb]
+        values = [*values, self.a0_val, self.wb_val]
+
+        for s, v in zip(symbols, values):
+            self.h = self.h.subs(s, v)
+        if s_vals is None:
+            return self.h
+        
+        func = sp.lambdify(self.s, self.h, 'numpy')
+        return func(s_vals)
+    
+    def plot(self, f, symbols, values):
+        s_val = 2*np.pi*f*1j
+        h_data = opamp.eval(symbols, values, s_val)
+        plotBode(h_data, f)
 
 
 R, C, r = sp.symbols('R C r', positive=True, real=True)
+RCr_values = [5100, 10*1e-9, 5100]
+
 s = sp.symbols('s', real=False)
 
 # Define the equation to be simplified
@@ -59,5 +79,19 @@ k_der_comp = -s*R*C/(1 + s*r*C)
 k_int_comp = -r/(R*(1 + s*r*C))
 
 opamp = OpAmp(s)
-opamp.set_ki(k_der)
+opamp.set_ki(k_int)
+
+print("h: ", opamp.pretty())
+
+a0 = opamp.a0
+wb = opamp.wb
+eq = (C*R*a0*wb + C*R*wb + 1)/wb
+# Simplify the equation
+eq_s = sp.simplify(eq)
+print("eq_s: ", eq_s)
+
 opamp.pretty_print()
+
+# Define a logspace vector for the frequency used with the s complex variable
+# frec = np.logspace(-4, 6, 5000)
+# opamp.plot(frec, [R, C, r], RCr_values)
--- utils/plotTools.py
+++ utils/plotTools.py
@@ -1,34 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from plotUtils import getGainPhase
 
-def plotBode(e1, e2, H, points = 1000):
-    w = np.logspace(e1, e2, points)
-
-    # evaluate the complex function at each frequency
-    s = 1j * w
-    Hs = H(s)
-    gain = 20 * np.log10(np.abs(Hs))
+def plotBode(h_data, frec):
+    gain, phase = getGainPhase(h_data)
 
     # Create a figure and two axes
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
 
-    ax1.semilogx(w, gain, color='tab:red', linestyle='-')
-    ax1.set_ylabel('Magnitude')
+    ax1.semilogx(frec, gain, color='tab:red', linestyle='-')
+    ax1.set_ylabel('Ganancia [dB]')
 
-    ax2.semilogx(w, np.angle(Hs, deg=True), lw=2.5 , color='tab:blue', linestyle='--')
-    ax2.set_xlabel('Frequency (rad/s)')
-    ax2.set_ylabel('Phase (deg)')
+    ax2.semilogx(frec, phase, lw=2.5 , color='tab:blue', linestyle='-')
+    ax2.set_xlabel('Frecuencia $[Hz]$')
+    ax2.set_ylabel('Fase $[\degree]$')
 
     ax1.tick_params(axis='y', labelcolor='tab:red')
     ax1.grid(True, which="both", ls="-", axis="x")
     ax1.grid(True, which="both", ls="-", axis="y")
 
     ax2.tick_params(axis='y', labelcolor='tab:blue')
-    ax2.grid(True, which="major", ls="--", color='black', alpha=1.0)
+    ax2.grid(True, which="major", ls="--")
 
     ax1.xaxis.set_minor_locator(plt.LogLocator(base=10, subs='all', numticks=100))
     ax1.xaxis.set_major_locator(plt.LogLocator(base=10, numticks=30))
+
+    # set ticks for ax2 y axis
+    ax2.yaxis.set_major_locator(plt.MultipleLocator(10))
 
     plt.tight_layout()
     plt.show()
